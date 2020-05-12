d06d32ea6c199555753b78aa183b256c6bceefea
Scott Morgan <smorgan@bridgend.ac.uk>
2020-05-12 13:48:13 +0100

    Update test_exercise.py

--- tests/test_exercise.py
+++ tests/test_exercise.py
@@ -1,7 +1,9 @@
 import pytest
-import src.exercise
+from src.exercise import main
+import os
 
-def test_exercise():
+def test_exercise(capsys):
+    os.chdir('src')
     main()
     out, err = capsys.readouterr()
     assert out == "In a\nworld\n"
